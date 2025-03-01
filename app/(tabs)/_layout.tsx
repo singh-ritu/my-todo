a1f92aa4f1827e6dc7693b86a8ea81849a8d0f0b
singh-ritu <your-email@example.com>
2025-03-01 19:42:59 +0530

    feat: Login/SignUp ui is created

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import "../../global.css";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -14,14 +14,16 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: "#3b4270",
+        tabBarInactiveTintColor: "#555",
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
+            // backgroundColor: "#1E1E1E",
           },
           default: {},
         }),
@@ -29,15 +31,19 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "SignUp",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "SignIn",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="key.fill" color={color} />
+          ),
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,84 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Text,
+  View,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { TextInput } from "react-native-paper";
 
 export default function HomeScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const router = useRouter();
+  const handleSignup = () => {
+    console.log("logged details");
+  };
   return (
-    <View className="flex-1 items-center justify-center bg-blue-500">
-      <Text className="text-white text-xl font-bold">Hello, World!</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/Surf.jpeg")} // Ensure image is inside "assets" folder
+      className="flex-1 justify-end items-center mb-8"
+      resizeMode="cover" // Ensures the image covers the entire screen
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}>
+        <View className=" flex w-screen justify-center align-middle rounded-2xl p-4 bg-[white] m-4">
+          <Text className="text-xl font-bold m-4 text-[black] text-center">
+            Create your First Todo!
+          </Text>
+
+          <View className="my-1">
+            <TextInput
+              label="Enter Your Name"
+              value={name}
+              keyboardType="default"
+              onChangeText={setName}
+              mode="outlined"
+            />
+          </View>
+          <View className="my-1">
+            <TextInput
+              label="Enter Your Email"
+              value={email}
+              keyboardType="email-address"
+              onChangeText={setEmail}
+              mode="outlined"
+            />
+          </View>
+          <View className="my-1">
+            <TextInput
+              label="Enter Your Password"
+              value={password}
+              secureTextEntry
+              onChangeText={setPassword}
+              mode="outlined"
+            />
+          </View>
+          <TouchableOpacity onPress={handleSignup}>
+            <Text className="text-lg text-center rounded-lg my-6 text-white font-bold p-4 bg-[#1b3552]">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+
+          <Text className="text-lg text-center font-semibold">
+            Already have an account?{" "}
+            <Text
+              className="text-blue-400"
+              onPress={() => {
+                console.log("Signin clicked");
+                router.replace("/explore");
+              }}>
+              Sign In
+            </Text>
+          </Text>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
   );
 }
-
-// s
